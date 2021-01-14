4821f344aca9c14dd7c15cce411e902b8a4e6572
Miguel Ramos <miguel.ramos@prozis.com>
2021-01-14 19:59:52 +0000

    chore: init create command

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,19 +1,29 @@
+import { red } from 'chalk';
 import type { Command } from 'commander';
 
-import { validateEmptyValue } from '../utils';
+import { getCommandWorkDir } from '../utils';
+
+enum TYPE {
+  'LIB',
+  'APP'
+}
+
+async function initCreate(command: Command & Commander) {
+  if (!command.app || !command.lib) {
+    console.error(red('🚨 Please choose argument: --app or --lib to create.'));
+    process.exit(1);
+  }
+
+  command.app ? TYPE.APP : TYPE.LIB;
+}
 
 export function createCommand(command: Commander & Command) {
   command
     .command('create')
     .description('Create application or library')
     .option('--app <name>', 'Create application')
+    .option('--lib <name>', 'Create library')
     .option('--type <framework>', 'Type of framework', 'vue')
-    .action((command: Commander) => {
-      const appConfig = new Map();
-
-      validateEmptyValue(command.app);
-
-      appConfig.set('app', command.app);
-      appConfig.set('type', command.type);
-    });
+    .option('--base', 'Directory to create it', getCommandWorkDir())
+    .action(initCreate);
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,7 @@
 interface Commander {
   init: string;
   app: string;
+  lib: string;
   type: string;
   project: string;
   base: string;
