72ca5886121a6b63d6e61c181dda6980a0c417ac
Miguel Ramos <miguel.ramos@prozis.com>
2021-01-14 18:17:55 +0000

    chore: clean bin folder

--- bin/tester/postcss.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
-
-module.exports = {
-  plugins: [tailwindcss, autoprefixer]
-};
